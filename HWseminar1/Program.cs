713427e6b1c662e3bf1181ff0c7bf99bae737591
Guccher <Guccher_007@mail.ru>
2022-08-12 15:58:40 +0300

    hw_seminar1

---
+++ HWseminar1/Program.cs
@@ -0,0 +1,72 @@
+﻿//   Задача №2
+
+//  Console.Write("Input Count a = ");
+//  int a = Convert.ToInt32(Console.ReadLine());
+//  Console.Write("Input Count b = ");
+//  int b = Convert.ToInt32(Console.ReadLine());
+
+//  if (a > b){
+//      Console.Write("Max count = ");
+//  Console.WriteLine(a);
+//  }
+//  else
+//  {
+//      Console.Write("Max count = ");
+//      Console.WriteLine(b);
+//  }
+
+//  Задача №4
+
+//  Console.Write("Input Count a = ");
+//  int a = Convert.ToInt32(Console.ReadLine());
+//  Console.Write("Input Count b = ");
+//  int b = Convert.ToInt32(Console.ReadLine());
+//  Console.Write("Input Count с = ");
+//  int c = Convert.ToInt32(Console.ReadLine());
+//  int max=a;
+//  if (a > max)
+//  max = a;
+//  if (b > max)
+//  max = b;
+//  if (c > max)
+//  max = c;
+//  Console.Write("max count = ");
+//  Console.WriteLine(max);
+
+//  Задача №6
+
+//  Console.Write("Input number: ");
+//  int number = Convert.ToInt32(Console.ReadLine());
+//  if (number % 2 == 0){
+//  Console.Write("the number ");
+//  Console.WriteLine(number);
+//  Console.Write("is even");
+//  }
+//  else
+//   {
+//  Console.Write("the number "); 
+//  Console.WriteLine(number); 
+//  Console.Write("is not even");
+//  }
+
+//  Задача №8
+
+// Console.Write("Input count: ");
+// int count = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// while (count < number){
+//     if (count % 2 == 0){ 
+//     Console.WriteLine(count); 
+//     }
+//    count ++;
+// }
+
+// задача №7
+
+// Console.Write("Input number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input number3: ");
+// int number3 = Convert.ToInt32(Console.ReadLine()); // не знаю как здесь присвоить переменной "0", поэтому ввожу его с клавиатуры (с верхней задачей аналогично)
+// number3 = number % 10;
+// Console.WriteLine(number3);
